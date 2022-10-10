cba6c7e8eded63db83682b5542c0b994f4e23c38
Gandalf329 <pohlebaev.n@yandex.ru>
2022-10-10 10:41:15 +0300

    Edit send messages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using SignalRMVC.Models;
 using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SignalRMVC.Controllers
 {
@@ -9,16 +10,43 @@ namespace SignalRMVC.Controllers
     {
 
         ApplicationContext db;
-        public HomeController(ApplicationContext context)
+        UserManager<User> _userManager;
+        public HomeController(UserManager<User> userManager, ApplicationContext context)
         {
+            _userManager = userManager;
             db = context;
         }
 
         public async Task<IActionResult> Index()
         {
-            return View(await db.Messages.ToListAsync());
+            return View();
         }
+        public async Task<IActionResult> ViewUserMessages(string name, string name2)
+        {
+            User user = await _userManager.FindByNameAsync(name);
+            if (user == null)
+            {
+                //Console.WriteLine("user1 null");
+                return NotFound();
+            }
+            User user2 = await _userManager.FindByNameAsync(name2);
+            if (user2 == null)
+            {
+                //Console.WriteLine("user1 null");
+                return NotFound();
+            }
+            var messages = from m in db.Messages select m;
+            if (!String.IsNullOrEmpty(name) || !String.IsNullOrEmpty(name2))
+            {
+                messages = messages.Where(s => (s.UserName.Contains(name) || s.ReceiverName.Contains(name)) && (s.UserName.Contains(name2) || s.ReceiverName.Contains(name2)));
+            }
 
+            return View(await messages.ToListAsync());
+        }
+        public async Task<IActionResult> AllUsers()
+        {
+            return View(await _userManager.Users.ToListAsync());
+        }
         public IActionResult Privacy()
         {
             return View();
