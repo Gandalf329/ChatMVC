abeab0f0dc57f1c09a92e6e805525d2f7d8531ba
Gandalf329 <pohlebaev.n@yandex.ru>
2022-10-21 11:44:22 +0300

    Update send message

--- SignalR/ChatHub.cs
+++ SignalR/ChatHub.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SignalRMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Xml.Linq;
 
 namespace SignalRMVC.SignalR
 {
     public class ChatHub : Hub
     {
         ApplicationContext db;
-        public ChatHub(ApplicationContext context)
+        UserManager<User> _userManager;
+        public ChatHub(UserManager<User> userManager, ApplicationContext context)
         {
+            _userManager = userManager;
             db = context;
         }
         public async Task Send(string message, string userName, string receiverName)
@@ -15,7 +19,13 @@ namespace SignalRMVC.SignalR
             Message message1 = new Message { ReceiverName = receiverName, UserName = userName, MessageText = message };
             await db.Messages.AddAsync(message1);
             await db.SaveChangesAsync();
-            await Clients.All.SendAsync("Receive", message, userName);
+
+            User user = await _userManager.FindByNameAsync(receiverName);
+            string receiver = user.Id;
+
+            //await Clients.All.SendAsync("Receive", message, userName);
+            await Clients.Users(Context.UserIdentifier, receiver).SendAsync("Receive", message, userName);
+
         }
     }
 }
