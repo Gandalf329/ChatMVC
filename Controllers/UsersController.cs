d2cfe7e99576cf2b887715b45fb2174baa48ae92
Gandalf329 <pohlebaev.n@yandex.ru>
2022-10-30 22:43:45 +0300

    Add user profile

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SignalRMVC.Models;
+
+namespace SignalRMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserManager<User> _userManager;
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> UserProfile(string name)
+        {
+            User user = await _userManager.FindByNameAsync(name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+    }
+}
